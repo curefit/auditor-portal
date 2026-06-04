99f4f771987bcf1f03427a5ec53baa2aabc0afb8
Shivalingesh Aiholli <shivalingesh.aiholli@curefit.com>
2026-06-04 13:20:51 +0530

    changes to 3 queries (#20)

--- artifacts/queries/74738__how-much-data-from-wearbles-v3.sql
+++ artifacts/queries/74738__how-much-data-from-wearbles-v3.sql
@@ -3,7 +3,8 @@ SELECT month,
        COUNT(DISTINCT userid) users,
        avg(days) days
        
-FROM(
+FROM
+(
 select DATE_TRUNC('month',date(ac.createddate_date)) month,
         activitytype,
        "type",
@@ -14,9 +15,15 @@ select DATE_TRUNC('month',date(ac.createddate_date)) month,
 
 FROM pk_prod_curefit_prod.activitystores ac
 
-left JOIN  pk_prod_curefit_prod.activitystores_meta_healthmetric ashm on ashm.root_ref_id = ac._id and date(ashm.root_createddate_date) = date(ac.createddate_date) and date(ashm.root_createddate_date) >= DATE('2025-01-01')
-LEFT JOIN pk_prod_curefit_prod.activitystores_meta_healthmetric_healthmetricinstance ashmi ON ashmi.root_ref_id = ac._id and ashmi.parent_array_index = ashm.array_index and date(ashm.root_createddate_date) = date(ashmi.root_createddate_date) and date(ashmi.root_createddate_date) >= DATE('2025-01-01')
-where activitytype IN ('WALK','SLEEP','CALORIES_BURNT','HEART_RATE','HEART_RATE_VARIABILITY','HEALTH_METRIC_TIME_SERIES','RESTING_HEART_RATE') 
+left JOIN  pk_prod_curefit_prod.activitystores_meta_healthmetric ashm on ashm.root_ref_id = ac._id 
+and date(ashm.root_createddate_date) = date(ac.createddate_date) 
+and date(ashm.root_createddate_date) BETWEEN {{start}} AND {{end}}
+
+LEFT JOIN pk_prod_curefit_prod.activitystores_meta_healthmetric_healthmetricinstance ashmi ON ashmi.root_ref_id = ac._id and ashmi.parent_array_index = ashm.array_index and date(ashm.root_createddate_date) = date(ashmi.root_createddate_date) 
+and date(ashmi.root_createddate_date) BETWEEN {{start}} AND {{end}}
+
+where 
+activitytype IN ('WALK','SLEEP','CALORIES_BURNT','HEART_RATE','HEART_RATE_VARIABILITY','HEALTH_METRIC_TIME_SERIES','RESTING_HEART_RATE') 
 and (date IS NOT NULL OR value IS NOT NULL)
 and date(ac.createddate_date) BETWEEN {{start}} AND {{end}}
 GROUP BY 1,2,3,4
