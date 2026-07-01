fde9e5cecdfd364d21d7f12c805f80369a41cac1
AiholliShiva <shivalingesh.aiholli@curefit.com>
2026-07-01 21:53:29 +0530

    Refresh pack pricing query and result artifacts

--- artifacts/queries/86316__pack-pricing-dump-31st-march.sql
+++ artifacts/queries/86316__pack-pricing-dump-31st-march.sql
@@ -1,1 +1,1 @@
-SELECT * FROM stage_dwh.dwh_finance_consolidated_ledger_v2 WHERE order_created_date = DATE '2026-03-31'
+SELECT * FROM dwh_finance.consolidated_ledger_v2 WHERE order_created_date = DATE '2026-03-31'
